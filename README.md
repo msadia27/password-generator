a6db1a79551e7c356f46668658711e88a0762791
Madelane Sadia <madelanecsadia@gmail.com>
2022-08-04 17:47:23 -0400

    alerts and prompts

--- README.md
+++ README.md
@@ -8,16 +8,30 @@ SO THAT I can create a strong password that provides greater security
 ACCEPTANCE CRITERIA
 GIVEN I need a new, secure password
 WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
+THEN I am presented with a series of prompts for password criteria [(1) password ]
+
 WHEN prompted for password criteria
 THEN I select which criteria to include in the password
+[user must select atleast 1 criteria]
+
 WHEN prompted for the length of the password
 THEN I choose a length of at least 8 characters and no more than 128 characters
+
+var length: 8-128
+
 WHEN asked for character types to include in the password
 THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+
+var lowercase 
+var uppercase
+var numeric
+var special
+
 WHEN I answer each prompt
 THEN my input should be validated and at least one character type should be selected
+
 WHEN all prompts are answered
 THEN a password is generated that matches the selected criteria
+
 WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page# password-generator
--- develop/script.js
+++ develop/script.js
@@ -1,17 +1,31 @@
 // Assignment code here
+var length; //length of password (numbers 8-128)
+var lowerCase; //lowercase
+var upperCase; //upper case
+var number; //numbers
+var specialCase; //special character
 
+//arrays
+alphabetLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
+alphabetUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+number = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+special = [" ", "!", "#", "$", "$", "%", "&", "'", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "=", "?", "@", "[", "]", "_", "{", "}", "|", "~"];
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+//generatePassword function 
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function generatePassword() {
 
-  passwordText.value = password;
+  //alerts/prompts
+  var length = window.prompt("Please enter a number betweek 8 and 128 for password length.");
+  
+  if (length < 8 || length > 128) {
+    window.alert("Character length requirement was not met.  Please enter a number betweek 8 and 128 for password length.");
+  }
 
-}
+  var lowerCase = window.prompt("Do you want to include lower case letters?");
+  var upperCase = window.prompt("Do you want to include upper case letters?");
+  var number = window.prompt("Do you want to includes numbers?");
+  var specialCase = window.prompt("Do you want to include special characters?");
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+};
+
